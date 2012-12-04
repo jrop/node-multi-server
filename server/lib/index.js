d3e602ef7a6efce58ade74dfb2c7fb7af8f9e91e
Jonathan Apodaca <jrapodaca@gmail.com>
2012-12-03 20:35:52 -0600

    Fixed errors introduced from earlier commit.

--- server/lib/index.js
+++ server/lib/index.js
@@ -7,10 +7,4 @@ module.exports = {
 	Request : require('./request.js'),
 	Response : require('./response.js'),
 	Util : require('./util.js'),
-	tpl : {
-		T : require('./tpl/t.js'),
-		EjsT : require('./tpl/ejst.js'),
-		TManager : require('./tpl/tmanager.js'),
-		Inflator : require('./tpl/inflator.js')
-	}
 };
